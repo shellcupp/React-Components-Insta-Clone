15e4eb06944a68dab1669673649792e3e275636d
Michelle Cupp <shellcupp@gmail.com>
2020-01-21 14:10:02 -0600

    search bar still not popping up

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
+import PostsPage from "../src/components/PostsContainer/PostsPage";
+import SearchBar from "../src/components/SearchBar/SearchBarContainer";
+import "../src/components/SearchBar/SearchBar.css"
 import "./App.css";
+import dummydata from "./dummy-data"
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  const [data] = useState(dummydata);
+  console.log(data);
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+      {/* Add components here  and pass props where appropriate */
+      
+      }
     </div>
   );
 };
